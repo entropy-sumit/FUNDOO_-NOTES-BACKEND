e5febd0e8259c9206afaeb29566ecaada36c6cb8
entropy-sumit <sumitr600@gmail.com>
2022-03-01 00:40:35 +0530

    [add] sumit added Notes model and Db

---
+++ CommonLayer/UserNotes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class UserNotes
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsPinned { get; set; }
+        public bool Delete { get; set; }
+        public bool Archieve { get; set; }
+        public DateTime? Reminder { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? ModifiedTime { get; set; }
+        public string Color { get; set; }
+        public string BgImage { get; set; }
+    }
+}
--- RepositoryLayer/Entities/User.cs
+++ RepositoryLayer/Entities/User.cs
@@ -13,19 +13,20 @@ namespace RepositoryLayer.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
-        //[Required]
-        //[RegularExpression(@"^[A-Z]{1}[a-z]{3}$")]
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{3,}$")]
         public string FirstName { get; set; }
-        //[Required]
-        //[RegularExpression(@"^[A-Z]{1}[a-z]{3}$")]
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{3,}$")]
         public string LastName { get; set; }
-        //[Required]
-        //[RegularExpression(@"^[a-c]{3}[.+-_]{0,1}[x-z]{3}@[a-z]{2}[.+-]{0,1}[a-z]{2}[.+-]{0,1}[a-z]{2}$")]
+        [Required]
+        [RegularExpression(@"^[a-c]{3}[.+-_]{0,1}[x-z]{3}@[a-z]{2}[.+-]{0,1}[a-z]{2}[.+-]{0,1}[a-z]{2}$")]
         public string Email { get; set; }
-        //[Required]
-        //[RegularExpression(@"^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))")]
+        [Required]
+        [RegularExpression(@"^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))")]
         public string Password { get; set; }
         public DateTime? CreatedAt { get; set; } // ? is used to make nullable  
         public DateTime? ModifiedAt { get; set; }
+        public ICollection<Notes> Notes { get; set; }
     } 
 }
